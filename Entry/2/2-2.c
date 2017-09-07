232a9a0cd983af855bf8f56f906fcefd31a85eb2
minm923 <minm923@126.com>
2017-09-07 23:28:18 +0800

    promote 2-2.c

--- Entry/2/2-2.c
+++ Entry/2/2-2.c
@@ -5,17 +5,22 @@ int main(int argc, char * argv[])
     int n = 0;
     scanf("%d", &n);    
 
+    int i = 0;
+
     while(n != 1)
     {
         if (1 == n%2)        
         {
-
+            n = 3*n+1;
         }
         else
         {
-
+            n = n / 2;
         }
+        ++i;
     }
+
+    printf("%d\n", i);    
 
     return 0;    
 }
