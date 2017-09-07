ed590bb8c7e4d911aa2ae7824789d63785820043
minm923 <minm923@126.com>
2017-09-07 22:55:49 +0800

    第二章

---
+++ Entry/1-2/2-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    // 循环
+    int n = 1;
+    //scanf("%d", &n);
+
+    for(int i=1; i<=n; ++i)
+    {
+        printf("%d\n", i);
+    }
+
+    // 完全平方数
+    for (int i=1000; i<10000; ++i)
+    {
+        if ((i/1000 == i%1000/100) && (i%100/10 == i%100%10%10))
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;    
+}
---
+++ Entry/1-2/2-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    int n = 0;
+    scanf("%d", &n);    
+
+    while(n != 1)
+    {
+        if (1 == n%2)        
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    return 0;    
+}
