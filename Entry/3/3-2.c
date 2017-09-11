4fd2a47b7a524b32174cf7e8fbec02455129825c
minm923 <minm923@126.com>
2017-09-11 10:54:29 +0800

    fin 2

--- Entry/3/3-2.c
+++ Entry/3/3-2.c
@@ -7,29 +7,25 @@
 
 int arr[8][8];
 
-
 int main(int argc, char * argv[])
 {
-
     int n = 0;
     scanf("%d", &n);
     n = n % 8;
     memset(arr, 0, sizeof arr);
 
-
     int sum = n * n;
-    
-    int x = n -1;
-    int y = 0;
-    
-    int i = 1;
+    int x = 0;
+    int y = n-1;
 
+    arr[x][y] = 1;
+    int i = 2;
     while(i <= sum)
     {
-        while(y+1 < n  && !arr[x][y+1]) arr[x][y++] = i++;
-        while(x-1 > -1 && !arr[x-1][y]) arr[x--][y] = i++;
-        while(y-1 > -1 && !arr[x][y-1]) arr[x][y--] = i++;
-        while(x+1 < n  && !arr[x+1][y]) arr[x++][y] = i++;
+        while(x+1 < n   && !arr[x+1][y]) arr[++x][y] = i++;
+        while(y-1 > -1  && !arr[x][y-1]) arr[x][--y] = i++;
+        while(x-1 > -1  && !arr[x-1][y]) arr[--x][y] = i++;
+        while(y+1 < n   && !arr[x][y+1]) arr[x][++y] = i++;
     }
 
     for (int i = 0; i<n; ++i)
@@ -40,8 +36,6 @@ int main(int argc, char * argv[])
         }
         printf("\n");
     }
-
-
 
     return 0;
 }
