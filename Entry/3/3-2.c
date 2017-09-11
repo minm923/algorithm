5d7b6ff373845f8dd2cf85f77bb2ceec1e1bcd31
minm923 <minm923@126.com>
2017-09-11 09:08:40 +0800

    bugs

--- Entry/3/3-2.c
+++ Entry/3/3-2.c
@@ -13,14 +13,35 @@ int main(int argc, char * argv[])
 
     int n = 0;
     scanf("%d", &n);
+    n = n % 8;
+    memset(arr, 0, sizeof arr);
+
+
     int sum = n * n;
     
     int x = n -1;
     int y = 0;
+    
+    int i = 1;
 
-    for (int i = 1; i<=sum; ++i)    
+    while(i <= sum)
     {
+        while(y+1 < n  && !arr[x][y+1]) arr[x][y++] = i++;
+        while(x-1 > -1 && !arr[x-1][y]) arr[x--][y] = i++;
+        while(y-1 > -1 && !arr[x][y-1]) arr[x][y--] = i++;
+        while(x+1 < n  && !arr[x+1][y]) arr[x++][y] = i++;
     }
+
+    for (int i = 0; i<n; ++i)
+    {
+        for (int j=0; j<n; ++j)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
     return 0;
 }
