e7555ac72018730c23433ed19f102a46bbbaf27a
minm923 <minm923@126.com>
2017-09-25 11:30:13 +0800

    fin 5-6

--- Entry/5/ep5-6.cc
+++ Entry/5/ep5-6.cc
@@ -4,34 +4,70 @@
 #include <cctype>
 #include <set>
 #include <queue>
-#include <deque>
+#include <map>
+
+const int TEAM_NUM = 1000;
 
 using namespace std;
 
+map<int, int> PlayerIDMapTeamID;
+
 int main(int argc, char * argv[])
 {
-    deque<int> qi;
+    int n = 0;
+    cin >> n;   
+    for (int i=0; i<n; ++i)
+    {
+        int iPlayerNum = 0;        
+        cin >> iPlayerNum;
+        for (int j=0; j<iPlayerNum; ++j)
+        {
+            int iPlayerID = 0;
+            cin >> iPlayerID;
+            PlayerIDMapTeamID[iPlayerID] = i;
+        }
+    }
+
+    queue<int> TeamQueue;
+    queue<int> Team[TEAM_NUM];
+
+    string op;    
+    while(cin >> op)        
+    {
+        if ('E' == op[0])
+        {
+            int iPlayerID = 0;
+            cin >> iPlayerID;
+            int iTeamID = PlayerIDMapTeamID[iPlayerID];
+
+            if (Team[iTeamID].empty())
+            {
+                TeamQueue.push(iTeamID);
+            }
+
+            Team[iTeamID].push(iPlayerID);
+        }
+        else if ('D' == op[0])
+        {
+            const int iTeamID   = TeamQueue.front();
+            const int iPlayerID = Team[iTeamID].front();
+            cout << iPlayerID << endl;
+
+            Team[iTeamID].pop();
+            if (Team[iTeamID].empty())
+            {
+                TeamQueue.pop();
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // printf
     
-    for (int i=0; i<10; ++i)
-    {
-        qi.push_back(i);
-    }
-
-    cout << qi.front() << endl;
-    cout << qi.back()  << endl;
-    cout << qi.size()  << endl;
-    qi.pop_front();
-    cout << qi.front() << endl;
-    cout << qi.size()  << endl;
-
-    cout << "-------" << endl;
-
-    for (deque<int>::iterator it=qi.begin(); it!=qi.end(); ++it)
-    {
-        cout << *it << endl;
-    }
-
-
 
     return 0;    
 }
+
