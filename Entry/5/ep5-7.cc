a4350a25fee5ad439243267b294eeaef8b0755ca
minm923 <minm923@126.com>
2017-09-25 14:06:04 +0800

    fin ep5-7

---
+++ Entry/5/ep5-7.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <set>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+struct cmp
+{
+    // 个位数较小的在前
+    bool operator() (const int a, const int b) const 
+    {
+        return a%10 > b%10;        
+    }
+};
+
+priority_queue<int, vector<int>, cmp> pq;
+
+int main(int argc, char * argv[])
+{
+    pq.push(11);    
+    pq.push(9);    
+    pq.push(8);    
+    pq.push(2);    
+    pq.push(5);    
+
+    while(!pq.empty())
+    {
+        const int i = pq.top();        
+        cout << i << endl;
+        pq.pop();
+    }
+
+    return 0;    
+}
+
