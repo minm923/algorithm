adfc4c8eb78a5985b005801c9750b772ad8ea956
minm923 <minm923@126.com>
2017-10-10 21:15:21 +0800

    listnode

---
+++ Offer/2/2.3/list.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+struct ListNode
+{
+    int m_nValue;
+    struct ListNode * m_pNext;
+};
+
+void AddToTail(struct ListNode** pHead, int value)
+{
+    struct ListNode * pNewNode = new struct ListNode(); 
+
+    if (pHead != NULL && pNewNode !=NULL)
+    {
+        pNewNode->m_nValue = value;
+        pNewNode->m_pNext  = NULL;
+
+        if (*pHead == NULL)
+        {
+            *pHead = pNewNode;
+        }
+        else
+        {
+            struct ListNode * tmp = *pHead;
+            while (tmp->m_pNext)
+            {
+                tmp = tmp->m_pNext;
+            }
+
+            tmp->m_pNext = pNewNode;
+        }
+    }
+}
+
+int main(int argc, char * argv[])
+{
+
+
+    
+    return 0;
+}
