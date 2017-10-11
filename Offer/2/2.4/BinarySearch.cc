3de0996957378ec4f4ad679d57ae8dc43c772357
minm923 <minm923@126.com>
2017-10-11 12:56:52 +0800

    add BinarySearch

---
+++ Offer/2/2.4/BinarySearch.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int BinarySearch(int* arr, int low, int high, int value)
+{
+    if (arr != NULL && low>=0 && high>=0 && high >=low)
+    {
+        int mid  = 0;
+        long sum = 0;
+        while (low <= high)         
+        {
+            sum = low + high;                
+            mid = sum / 2;
+
+            if (arr[mid] > value)
+            {
+                high = mid -1;                                
+            }
+            else if (arr[mid] < value)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main(int argc, char * argv[])
+{
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    std::cout << BinarySearch(arr, 0, 9, 8) << std::endl;
+
+    return 0;
+}
