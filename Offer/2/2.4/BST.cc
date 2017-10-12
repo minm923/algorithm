e29875ef7135e6418b787d1f53f058a77c6ad655
minm923 <minm923@126.com>
2017-10-12 09:57:39 +0800

    BST insert

--- Offer/2/2.4/BST.cc
+++ Offer/2/2.4/BST.cc
@@ -5,7 +5,7 @@ struct BSTNode
     int m_Value;
     BSTNode * p_left;
     BSTNode * p_right;    
-}
+};
 
 int SearchBST1(BSTNode* pNode, int value)
 {
@@ -17,11 +17,11 @@ int SearchBST1(BSTNode* pNode, int value)
         }
         else if (pNode->m_Value > value)
         {
-            return SearchBST(pNode->p_left, value);        
+            return SearchBST1(pNode->p_left, value);        
         }
         else
         {
-            return SearchBST(pNode->p_right, value);        
+            return SearchBST1(pNode->p_right, value);        
         }
     }
 
@@ -36,11 +36,11 @@ int SearchBST2(BSTNode* pNode, int value)
         {
             if (pNode->m_Value > value)
             {
-                pNode = pNode->left;
+                pNode = pNode->p_left;
             }
-            else if (pNode->mValue < value)
+            else if (pNode->m_Value < value)
             {
-                pNode = pNode->right;
+                pNode = pNode->p_right;
             }
             else
             {
@@ -57,85 +57,65 @@ int SearchBST2(BSTNode* pNode, int value)
     return (-value + 1);
 }
 
-int InsertBST1(BSTNode** pRoot, int value)
-{
-    if (pRoot)
-    {
-        if (!(*pRoot))
-        {
-            (*pRoot) = new BSTNode();
-            (*pRoot)->m_Value = value;
-            (*pRoot)->p_left  = NULL;
-            (*pRoot)->p_right = NULL;
-
-            return 1;
-        }
-        else
-        {
-            return InsertCore((*pRoot), value);
-        }
-    }
-}
-
-int InsertCore(BSTNode* pNode, int value)
-{
-    if (pNode->m_Value == value)
-    {
-        return 0;
-    }
-    else if (pNode->m_Value > value)
-    {
-        if (pNode->m_right)
-        {
-            return InsertCore(pNode->m_right, value);
-        }
-        else
-        {
-            pNode->m_right = new BSTNode();                        
-            pNode->m_right->m_Value = value;
-            pNode->m_right->p_left  = NULL;
-            pNode->m_right->p_right = NULL;
-
-            return 1;
-        }
-    }
-    else
-    {
-        if (pNode->left)
-        {
-            return InsertCore(pNode->left, value);
-        }
-        else
-        {
-            pNode->m_left = new BSTNode();                        
-            pNode->m_left->m_Value = value;
-            pNode->m_left->p_left  = NULL;
-            pNode->m_left->p_right = NULL;
- 
-            return 1;
-        }
-    }
-}
 
 int InsertBST2(BSTNode** pRoot, int value)
 {
+    BSTNode * pNode = (*pRoot);
+    int flg = 0;
+
     if (pRoot)
     {
-        if ((*pRoot) == NULL)
+        if (pNode == NULL)
         {
-            (*pRoot) = new BSTNode();
-            (*pRoot)->m_Value = value;
-            (*pRoot)->p_left  = NULL;
-            (*pRoot)->p_right = NULL;
-
+            pNode = new BSTNode();
+            pNode->m_Value = value;
+            pNode->p_left  = NULL;
+            pNode->p_right = NULL;
+            flg = 1;
         }
         else
         {
-
+            while(!flg)
+            {
+                if (pNode->m_Value > value)
+                {
+                    if (pNode->p_left == NULL)                    
+                    {
+                        pNode->p_left = new BSTNode();                                                                    
+                        pNode->p_left->m_Value = value;
+                        pNode->p_left->p_left  = NULL;
+                        pNode->p_left->p_right = NULL;
+                        flg = 1;
+                    }
+                    else
+                    {
+                        pNode = pNode->p_left;
+                    }
+                }
+                else if (pNode->m_Value < value)
+                {
+                    if (pNode->p_right == NULL)                    
+                    {
+                        pNode->p_right = new BSTNode();                                                                    
+                        pNode->p_right->m_Value = value;
+                        pNode->p_right->p_left  = NULL;
+                        pNode->p_right->p_right = NULL;
+                        flg = 1;
+                    }
+                    else
+                    {
+                        pNode = pNode->p_right;
+                    }
+                }
+                else
+                {
+                    flg = 0;
+                }
+            }
         }
     }
 
-    return 0;    
+    return flg;
 }
 
 int main(int argc, char * argv[])
