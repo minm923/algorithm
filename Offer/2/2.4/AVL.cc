14bab6687b6400ae1bd9c7483b2c2ffbd5c32137
minm923 <minm923@126.com>
2017-10-14 18:03:09 +0800

    add .

--- Offer/2/2.4/AVL.cc
+++ Offer/2/2.4/AVL.cc
@@ -1,5 +1,43 @@
 #include <iostream>
 
+#define HEIGHT(p) ( (p==NULL)?0:(((Node*)(p))->height) )
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+typedef int Type;
+
+typedef struct AVLTreeNode
+{
+    Type key;
+    int height;
+    AVLTreeNode * left;
+    AVLTreeNode * right;
+}Node, *AVLTree;
+
+Node* avltree_create_node (Type key, Node* left, Node* right)
+{
+    Node * p   = new AVLTreeNode();
+    if (!p)
+    {
+        p->key     = key;
+        p->height  = 0;
+        p->left    = left;
+        p->right   = right;
+    }
+    
+    return p;
+}
+
+int avltree_height(AVLTree tree)
+{
+    return HEIGHT(tree);
+}
+
+Node* avltree_insert(AVLTree tree, Type key)
+{
+
+}
+
 int main(int argc, char * argv[])
 {
 
