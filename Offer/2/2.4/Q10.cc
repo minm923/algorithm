ddd3969098f5032cd009ebfa81f5d71a22ed4d88
minm923 <minm923@126.com>
2017-10-22 13:21:01 +0800

    Q10

--- Offer/2/2.4/Q10.cc
+++ Offer/2/2.4/Q10.cc
@@ -24,7 +24,7 @@ int NumberOf1_2(int n)
     while (n)
     {
         ++count;
-        n = (n-1) & n;
+        n = (n-1) & n;// -1技巧
     }
     printf("count = %d\n", count);
     return count;    
