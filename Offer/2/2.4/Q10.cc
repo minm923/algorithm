7d8d42e6e908d8cf40352c48cf2842ae008ee2cc
minm923 <minm923@126.com>
2017-10-22 13:16:50 +0800

    Q10

---
+++ Offer/2/2.4/Q10.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int NumberOf1(int n)
+{
+    int count = 0;
+    int flg   = 1;
+
+    while (flg)
+    {
+        if (n & flg)
+            ++count;
+
+        flg = flg << 1;
+    }
+
+    printf("count = %d\n", count);
+    return count;    
+}
+
+int NumberOf1_2(int n)
+{
+    int count = 0;
+
+    while (n)
+    {
+        ++count;
+        n = (n-1) & n;
+    }
+    printf("count = %d\n", count);
+    return count;    
+}
+
+
+int main(int argc, char * argv[])
+{
+    NumberOf1(100);
+    NumberOf1(-100);
+
+    NumberOf1_2(100);
+    NumberOf1_2(-100);
+
+    return 0;    
+}
