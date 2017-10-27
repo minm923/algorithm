2f6712f7c5578f69174b43d58b43a283c7abf2c2
minm923 <minm923@126.com>
2017-10-27 11:11:16 +0800

    fin Q18

---
+++ Offer/3/3.4/Q18.cc
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int Arr1[N];
+int Arr2[N];
+
+struct BinaryTreeNode
+{
+    int m_nValue;
+    BinaryTreeNode* m_pLeft;
+    BinaryTreeNode* m_pRight;
+};
+
+bool DoesTree1HasTree2(BinaryTreeNode* pTree1, BinaryTreeNode* pTree2)
+{
+    if (NULL == pTree2)
+        return true;        
+
+    if (NULL == pTree1)
+        return false;
+
+    if (pTree1->m_nValue != pTree2->m_nValue)
+        return false;
+    
+    return DoesTree1HasTree2(pTree1->m_pLeft,  pTree2->m_pLeft) && 
+           DoesTree1HasTree2(pTree2->m_pRight, pTree2->m_pRight)             
+}
+
+bool HasSubTree (BinaryTreeNode* pTree1, BinaryTreeNode* pTree2)
+{
+    bool result = false;
+
+    if (pTree1 && pTree2)
+    {
+        if (pTree1->m_nValue == pTree2->m_nValue)
+        {
+            result = DoesTree1HasTree2(pTree1, pTree2);
+        }
+
+        if (!result)
+        {
+            result = HasSubTree(pTree1->m_pLeft, pTree2);
+        }
+
+        if (!result)            
+        {
+            result = HasSubTree(pTree1->m_pRight, pTree2);
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char * argv[])
+{
+
+    return 0;
+}
