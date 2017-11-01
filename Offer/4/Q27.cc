939f0d49579af8ff5cce3dc5e9c63338b37c4af8
minm923 <minm923@126.com>
2017-11-01 21:35:23 +0800

    Q27

--- Offer/4/Q27.cc
+++ Offer/4/Q27.cc
@@ -63,12 +63,73 @@ void InorderTraverse(BinaryTreeNode* pRoot)
     cout << endl;
 }
 
+void InorderTraverse2(BinaryTreeNode* pRoot)
+{
+    if (!pRoot)
+        return ;
+    vector<BinaryTreeNode*> ms;       
+    ms.push_back(pRoot);
+    BinaryTreeNode* pTop = NULL;
+
+    while (ms.size() > 0)
+    {
+        pTop = ms.back();
+        ms.pop_back();
+
+
+    }
+    cout << endl;
+}
+
+void MidorderTraverse(BinaryTreeNode* pRoot)
+{
+    if (!pRoot)
+        return ;    
+    int flg = 1;// push flg
+    vector<BinaryTreeNode*> ms;
+    ms.push_back(pRoot);
+    BinaryTreeNode* pTop = NULL;
+
+    while (ms.size() > 0)
+    {
+        pTop = ms.back();
+        if (flg)
+        {
+            if (pTop->m_pLeft)
+            {
+                ms.push_back(pTop->m_pLeft);
+            }
+            else
+            {
+                flg = 0;
+            }
+        }
+        else
+        {
+            cout << pTop->m_nValue << " ";
+            ms.pop_back();
+            if (pTop->m_pRight)
+            {
+                ms.push_back(pTop->m_pRight);
+                flg = 1;
+            }
+        }
+    }
+
+    cout << endl;
+}
+
+void PostorderTraverse(BinaryTreeNode* pRoot)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
     BinaryTreeNode* tree = NULL;
     CreateBiTree(&tree);
     InorderTraverse(tree);
+    MidorderTraverse(tree);
 
     return 0;
 }
-
