134f15ad22b1050dac90b8f6d2aa5c796650cb69
minm923 <minm923@126.com>
2017-11-03 15:04:07 +0800

    Q28去重

--- Offer/4/Q28.cc
+++ Offer/4/Q28.cc
@@ -11,8 +11,21 @@ void Swap(char* p1, char* p2)
         *p2      = tmp;
 }
 
+bool isSwap(char* pBegin, char* pEnd)
+{
+    for (char* pCh=pBegin; pCh < pEnd; ++pCh)
+    {
+        if (*pCh == *pEnd)
+            return false;
+    }
+    
+    return true;
+}
+
 void perm(char* pStr, char* pBegin)
 {
+    //printf("begin %c\n", *pBegin);
+    //if (isAllSame(pBegin) || *pBegin == '\0')
     if (*pBegin == '\0')
     {
         printf("%s\n", pStr);
@@ -21,9 +34,12 @@ void perm(char* pStr, char* pBegin)
     {
         for (char* pCh=pBegin; *pCh != '\0'; ++pCh)
         {
+            if (isSwap(pBegin, pCh))// attention  确定的位置和待交换的位置 之间有和待交换位置的元素相等的元素
+            {
                 Swap(pBegin, pCh);
                 perm(pStr, pBegin+1);
                 Swap(pBegin, pCh);
+            }
         }
     }
 }
@@ -44,7 +60,9 @@ int main(int argc, char* argv[])
     scanf("%s", str);    
     int iLen = strlen(str);
     printf("str : %s, len : %d\n", str, iLen);
+
     solution(str, iLen);
 
     return 0;
 }
+
