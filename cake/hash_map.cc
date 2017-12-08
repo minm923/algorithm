e8935581d76873009ee6d9066fffbf516876605b
minm923 <minm923@126.com>
2017-12-08 11:33:32 +0800

    hash_map

--- cake/hash_map.cc
+++ cake/hash_map.cc
@@ -1,7 +1,10 @@
 //g++ -o 1 hm.cc -Wno-deprecated
 #include <string>
 #include <iostream>
-#include <ext/hash_map>
+//#include <ext/hash_map>
+#include <hash_map>
+
+//typedef __gnu_cxx::hash_map<int, std::string> playerpool;
 
 typedef __gnu_cxx::hash_map<int, std::string> playerpool;
 
@@ -10,8 +13,8 @@ typedef __gnu_cxx::hash_map<int, std::string> playerpool;
 int main(int argc, char* argv[])
 {
     playerpool pool;
-    pool[1] = "gaomin";
-    pool[2] = "xuyue";
+    pool[1] = "xxxx";
+    pool[2] = "yyyy";
 
     std::cout <<  pool[1] << std::endl;
     std::cout <<  pool[2] << std::endl;
