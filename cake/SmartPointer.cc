6329f20b2ac4bca06d79825302955b76d9cc1225
minm923 <minm923@126.com>
2018-01-08 11:59:42 +0800

    My SmartPointer

---
+++ cake/SmartPointer.cc
@@ -0,0 +1,98 @@
+#include <iostream>
+
+template<typename T>
+class MySmartPointer
+{
+public:    
+    explicit MySmartPointer(T* const p)
+        : pT_(p),
+          pCount_(new int(1))
+    {
+
+    }
+
+    // rule of three
+    ~MySmartPointer() 
+    {
+        std::cout << "before dec count = " << getCount() << std::endl;
+        decrCount();
+        std::cout << "after dec count = " << getCount() << std::endl;
+        if (0 == getCount() )
+        {
+            std::cout << "releasePT" << std::endl;
+            releasePT();
+            releaseCount();
+        }
+    }
+
+    MySmartPointer(MySmartPointer& that)
+    {
+        pT_     = that.pT_;
+        pCount_ = that.pCount_;
+        incrCount();
+    }
+
+    MySmartPointer& operator=(const MySmartPointer& that)
+    {
+        pT_     = that.pT_;
+        pCount_ = that.pCount_;
+        incrCount();
+
+        return *this;
+    }
+
+    int  getCount()  { return *pCount_; }
+
+private:        
+    void releasePT()    { delete pT_; pT_ = NULL; }
+    void releaseCount() { delete pCount_; pCount_ = NULL; }
+
+    void incrCount() { ++(*pCount_); }
+    void decrCount() { --(*pCount_); }
+
+    T* pT_;
+    int* pCount_;
+};
+
+std::ostream& operator<<(std::ostream& os, MySmartPointer<int>& sp)
+{
+    os << sp.getCount();
+
+    return os;
+}
+
+void func(MySmartPointer<int> p)
+{
+    std::cout << "infunc " << std::endl;
+}
+
+void func2(MySmartPointer<int>& p)
+{
+    std::cout << "infunc2 " << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+    MySmartPointer<int> msp(new int(10));
+    std::cout << msp << std::endl;
+
+    func(msp);
+    std::cout << msp << std::endl;
+
+    func2(msp);
+    std::cout << msp << std::endl;
+
+    MySmartPointer<int> msp2(new int(10));
+    std::cout << msp2 << std::endl;
+    
+    MySmartPointer<int> msp3(msp2);
+    std::cout << msp3 << std::endl;
+
+    MySmartPointer<int> msp4 = msp3;
+    std::cout << msp4 << std::endl;
+
+    std::cout << "start deconstruct" << std::endl;
+
+    return 0;    
+}
+
